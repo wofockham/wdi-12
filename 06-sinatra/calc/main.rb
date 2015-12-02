18ce80b9ad2723bbe5f8122a4178bde8891d1d91
Cold Head <certainthings@gmail.com>
2015-12-02 12:58:15 +1100

    calculator with views

--- 06-sinatra/calc/main.rb
+++ 06-sinatra/calc/main.rb
@@ -4,3 +4,12 @@ require 'sinatra/reloader'
 get '/' do
   erb :home # Loads and serves up views/home.erb
 end
+
+get '/about' do
+  erb :about
+end
+
+get '/calc/multiply/:x/:y' do
+  @result = params[:x].to_f * params[:y].to_f
+  erb :calc
+end
