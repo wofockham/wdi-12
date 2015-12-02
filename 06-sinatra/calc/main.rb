880f8b672534519efbfb5ceaba45597eaf49b88a
Cold Head <certainthings@gmail.com>
2015-12-02 14:40:03 +1100

    calculator

--- 06-sinatra/calc/main.rb
+++ 06-sinatra/calc/main.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pry'
 
 get '/' do
   erb :home # Loads and serves up views/home.erb
@@ -9,7 +10,34 @@ get '/about' do
   erb :about
 end
 
+get '/calc' do
+  @x = params[:first].to_f
+  @y = params[:second].to_f
+
+  @result = case params[:operator]
+  when '+' then @x + @y
+  when '-' then @x - @y
+  when '*' then @x * @y
+  when '/' then @x / @y
+  end
+
+  erb :calc
+end
+
+
+
+
+
+
+
+
+
 get '/calc/multiply/:x/:y' do
   @result = params[:x].to_f * params[:y].to_f
+  erb :calc
+end
+
+get '/calc/add/:x/:y' do
+  @result = params[:x].to_f + params[:y].to_f
   erb :calc
 end
