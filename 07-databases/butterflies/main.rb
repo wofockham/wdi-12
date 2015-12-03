158298cb56ff26b3db454992fee8ac8b97d4968c
Cold Head <certainthings@gmail.com>
2015-12-04 10:56:33 +1100

    butterfly enhancements

--- 07-databases/butterflies/main.rb
+++ 07-databases/butterflies/main.rb
@@ -6,16 +6,14 @@ get '/' do
   erb :home
 end
 
-# Index page for all butterflies
+# Index page for all butterflies -- READ
 get '/butterflies' do
   @butterflies = query_db 'SELECT * FROM butterflies'
   erb :butterflies_index
 end
 
-get '/butterflies/new' do
-  erb :butterflies_new
-end
 
+# Insert a new butterfly into the database -- CREATE
 post '/butterflies' do
   query = "INSERT INTO butterflies (name, family, image) VALUES ('#{ params[:name] }', '#{ params[:family] }', '#{ params[:image] }')"
   query_db(query)
@@ -23,19 +21,26 @@ post '/butterflies' do
   redirect to('/butterflies')
 end
 
-# Show page for a butterfly
+# Form to add a new butterfly
+get '/butterflies/new' do
+  erb :butterflies_new
+end
+
+# Show page for a butterfly -- READ
 get '/butterflies/:id' do
-  butterflies = query_db "SELECT * FROM butterflies WHERE id=#{ params[:id] }"
+  butterflies = query_db "SELECT * FROM butterflies WHERE id=#{ params[:id].to_i }"
   @butterfly = butterflies.first # Strip off the array.
 
   erb :butterflies_show
 end
 
+# Delete a butterfly from the database -- DELETE
 get '/butterflies/:id/delete' do
-  query_db "DELETE FROM butterflies WHERE id=#{ params[:id] }"
+  query_db "DELETE FROM butterflies WHERE id=#{ params[:id].to_i }"
   redirect to('/butterflies')
 end
 
+# Form to edit an existing butterfly
 get '/butterflies/:id/edit' do
   butterflies = query_db "SELECT * FROM butterflies WHERE id=#{ params[:id] }"
   @butterfly = butterflies.first # Strip off the array.
@@ -43,6 +48,7 @@ get '/butterflies/:id/edit' do
   erb :butterflies_edit
 end
 
+# Amend a butterfly in the database -- UPDATE
 post '/butterflies/:id' do
   query = "UPDATE butterflies SET name='#{ params[:name] }', family='#{ params[:family] }', image='#{ params[:image] }' WHERE id=#{ params[:id] }"
   query_db query
