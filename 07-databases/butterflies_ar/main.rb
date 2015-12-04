1f7c05be8a28a0b1a44018ad07ed4db458893de1
Cold Head <certainthings@gmail.com>
2015-12-04 12:36:06 +1100

    plant create and read

--- 07-databases/butterflies_ar/main.rb
+++ 07-databases/butterflies_ar/main.rb
@@ -13,6 +13,13 @@ ActiveRecord::Base.establish_connection(
 class Butterfly < ActiveRecord::Base
 end
 
+class Plant < ActiveRecord::Base
+end
+
+after do
+  ActiveRecord::Base.connection.close
+end
+
 get '/' do
   erb :home
 end
@@ -26,8 +33,14 @@ end
 
 # Insert a new butterfly into the database -- CREATE
 post '/butterflies' do
-  query = "INSERT INTO butterflies (name, family, image) VALUES ('#{ params[:name] }', '#{ params[:family] }', '#{ params[:image] }')"
-  query_db(query)
+  # Buttefly.create :name => params[:name], :family => params[:family], :image => params[:image]
+
+  butterfly = Butterfly.new
+  butterfly.name = params[:name]
+  butterfly.family = params[:family]
+  butterfly.image = params[:image]
+
+  butterfly.save
 
   redirect to('/butterflies')
 end
@@ -45,38 +58,54 @@ end
 
 # Delete a butterfly from the database -- DELETE
 get '/butterflies/:id/delete' do
-  query_db "DELETE FROM butterflies WHERE id=#{ params[:id].to_i }"
+  butterfly = Butterfly.find params[:id]
+  butterfly.destroy
   redirect to('/butterflies')
 end
 
 # Form to edit an existing butterfly
 get '/butterflies/:id/edit' do
-  butterflies = query_db "SELECT * FROM butterflies WHERE id=#{ params[:id] }"
-  @butterfly = butterflies.first # Strip off the array.
-
+  @butterfly = Butterfly.find params[:id]
   erb :butterflies_edit
 end
 
 # Amend a butterfly in the database -- UPDATE
 post '/butterflies/:id' do
-  query = "UPDATE butterflies SET name='#{ params[:name] }', family='#{ params[:family] }', image='#{ params[:image] }' WHERE id=#{ params[:id] }"
-  query_db query
+  butterfly = Butterfly.find params[:id]
+  butterfly.name = params[:name]
+  butterfly.family = params[:family]
+  butterfly.image = params[:image]
+
+  butterfly.save
+
   redirect to("/butterflies/#{ params[:id] }")
 end
 
-def query_db(sql)
-  db = SQLite3::Database.new 'butterflies.db'
-  db.results_as_hash = true
+# Plant CRUD
 
-  puts sql
-
-  result = db.execute sql
-  result
+get '/plants' do
+  @plants = Plant.all
+  erb :plants_index
 end
 
+get '/plants/new' do
+  erb :plants_new
+end
 
+post '/plants' do
+  plant = Plant.new
+  plant.name = params[:name]
+  plant.image = params[:image]
 
+  plant.save
 
+  redirect to('/plants')
+end
+
+get '/plants/:id' do
+  @plant = Plant.find params[:id]
+  erb :plants_show
+end
 
 
 
