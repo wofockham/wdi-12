bfed6658d0ddc6f82d87550184ea608d561f23a1
Cold Head <certainthings@gmail.com>
2015-12-04 11:38:23 +1100

    active_record active_record active_record

---
+++ 07-databases/butterflies_ar/main.rb
@@ -0,0 +1,88 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sqlite3'
+require 'active_record'
+
+ActiveRecord::Base.logger = Logger.new(STDERR)
+
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3',
+  :database => 'butterflies.db'
+)
+
+class Butterfly < ActiveRecord::Base
+end
+
+get '/' do
+  erb :home
+end
+
+# Index page for all butterflies -- READ
+get '/butterflies' do
+  @butterflies = Butterfly.all
+  erb :butterflies_index
+end
+
+
+# Insert a new butterfly into the database -- CREATE
+post '/butterflies' do
+  query = "INSERT INTO butterflies (name, family, image) VALUES ('#{ params[:name] }', '#{ params[:family] }', '#{ params[:image] }')"
+  query_db(query)
+
+  redirect to('/butterflies')
+end
+
+# Form to add a new butterfly
+get '/butterflies/new' do
+  erb :butterflies_new
+end
+
+# Show page for a butterfly -- READ
+get '/butterflies/:id' do
+  @butterfly = Butterfly.find params[:id]
+  erb :butterflies_show
+end
+
+# Delete a butterfly from the database -- DELETE
+get '/butterflies/:id/delete' do
+  query_db "DELETE FROM butterflies WHERE id=#{ params[:id].to_i }"
+  redirect to('/butterflies')
+end
+
+# Form to edit an existing butterfly
+get '/butterflies/:id/edit' do
+  butterflies = query_db "SELECT * FROM butterflies WHERE id=#{ params[:id] }"
+  @butterfly = butterflies.first # Strip off the array.
+
+  erb :butterflies_edit
+end
+
+# Amend a butterfly in the database -- UPDATE
+post '/butterflies/:id' do
+  query = "UPDATE butterflies SET name='#{ params[:name] }', family='#{ params[:family] }', image='#{ params[:image] }' WHERE id=#{ params[:id] }"
+  query_db query
+  redirect to("/butterflies/#{ params[:id] }")
+end
+
+def query_db(sql)
+  db = SQLite3::Database.new 'butterflies.db'
+  db.results_as_hash = true
+
+  puts sql
+
+  result = db.execute sql
+  result
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
