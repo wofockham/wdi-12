9475cbba06f2d5f8457cb006720f6ead9c208b04
Cold Head <certainthings@gmail.com>
2015-12-08 15:27:52 +1100

    cruddy planets

--- 08-rails/solarsystem/app/controllers/planets_controller.rb
+++ 08-rails/solarsystem/app/controllers/planets_controller.rb
@@ -2,4 +2,38 @@ class PlanetsController < ApplicationController
   def index
     @planets = Planet.all
   end
+
+  def show
+    @planet = Planet.find params[:id]
+  end
+
+  def new
+  end
+
+  def create
+    planet = Planet.create planet_params
+    redirect_to planets_path
+  end
+
+  def destroy
+    planet = Planet.find params[:id]
+    planet.destroy
+    redirect_to planets_path
+  end
+
+  def edit
+    @planet = Planet.find params[:id]
+  end
+
+  def update
+    planet = Planet.find params[:id]
+    planet.update planet_params
+    redirect_to "/planets/#{ planet.id }"
+  end
+
+  private
+  # Strong Parameters
+  def planet_params
+    params.require(:planet).permit(:name, :image, :orbit, :diameter, :mass, :moons)
+  end
 end
--- 08-rails/solarsystem/config/routes.rb
+++ 08-rails/solarsystem/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   get '/planets' => 'planets#index'
+  get '/planets/new' => 'planets#new'
+  get '/planets/:id' => 'planets#show'
+  post '/planets' => 'planets#create'
+  delete '/planets/:id' => 'planets#destroy'
+  get '/planets/:id/edit' => 'planets#edit'
+  post '/planets/:id' => 'planets#update'
+
+  # resources :planets
 end
