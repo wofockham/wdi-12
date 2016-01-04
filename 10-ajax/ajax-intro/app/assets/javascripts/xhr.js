997f51b757369fcb9690bdd2a994547c1d99e807
Cold Head <certainthings@gmail.com>
2016-01-04 15:39:08 +1100

    fuller AJAX example with DOM interface

--- 10-ajax/ajax-intro/app/assets/javascripts/xhr.js
+++ 10-ajax/ajax-intro/app/assets/javascripts/xhr.js
@@ -2,12 +2,30 @@ $(document).ready(function () {
   $('#getLucky').on('click', function () {
 
     var request = new XMLHttpRequest();
-    request.open('GET', '/random');
+    request.open('GET', '/random'); // Set up where our request will be made to.
+
+    // Define a callback for when the request is done.
+    request.onreadystatechange = function () {
+      if (request.readyState !== 4) { return; }
+
+      // request.responseText is now available.
+      // THIS IS THE ONLY PLACE WHERE WE CAN USEFULLY ACCESS request.responseText
+      $('#result').text( request.responseText );
+    }
+
+    request.send(); // Initiate the request
+
+  });
+
+
+  $('#getTime').on('click', function () {
+
+    var request = new XMLHttpRequest();
+    request.open('GET', '/time');
 
     request.onreadystatechange = function () {
       if (request.readyState !== 4) { return; }
 
-      // request.responseText is now available
       $('#result').text( request.responseText );
     }
 
@@ -15,30 +33,3 @@ $(document).ready(function () {
 
   });
 });
-
-// var request = new XMLHttpRequest();
-// request.open('GET', '/time');
-
-// request.onreadystatechange = function () {
-//   if (request.readyState !== 4) {
-//     return;
-//   }
-
-//   console.log('The server time is', request.responseText);
-// };
-
-// request.send();
-
-// ///////////////////////////////////////////////////////////////////////////////
-// var request2 = new XMLHttpRequest();
-// request2.open('GET', '/random');
-
-// request2.onreadystatechange = function () {
-//   if (request2.readyState !== 4) {
-//     return; // Data is not ready yet.
-//   }
-
-//   console.log('Your lucky number is', request2.responseText);
-// };
-
-// request2.send();
