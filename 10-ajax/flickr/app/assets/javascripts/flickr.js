6d81fb7f98de142efd86fe144016bd90b4d83473
Cold Head <certainthings@gmail.com>
2016-01-06 16:37:38 +1100

    generating URLs

--- 10-ajax/flickr/app/assets/javascripts/flickr.js
+++ 10-ajax/flickr/app/assets/javascripts/flickr.js
@@ -1,5 +1,37 @@
 var searchFlickr = function (query) {
-  console.log(query);
+
+  var flickrUrl = 'https://api.flickr.com/services/rest/?jsoncallback=?';
+
+  $.getJSON(flickrUrl, {
+    method: 'flickr.photos.search',
+    api_key: '2f5ac274ecfac5a455f38745704ad084',
+    text: query,
+    format: 'json'
+  }).done(function (info) {
+    displayPhotos(info.photos.photo); // The actual photos are buried deep in the object.
+  });
+
+};
+
+var generateURL = function (photo) {
+  return [
+    'http://farm',
+    photo.farm,
+    '.static.flickr.com/',
+    photo.server,
+    '/',
+    photo.id,
+    '_',
+    photo.secret,
+    '_m.jpg' // Change this for different sizes.
+  ].join('');
+};
+
+var displayPhotos = function (photos) {
+  _.each(photos, function (photo) {
+    var photoURL = generateURL(photo);
+    console.log(photoURL);
+  })
 };
 
 $(document).ready(function () {
