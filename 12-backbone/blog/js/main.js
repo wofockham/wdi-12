b029ea8443f3b79679196de54bf0bc9e61e13979
Cold Head <certainthings@gmail.com>
2016-01-08 17:09:00 +1100

    backbone blog complete

--- 12-backbone/blog/js/main.js
+++ 12-backbone/blog/js/main.js
@@ -25,21 +25,99 @@ var Posts = Backbone.Collection.extend({
 });
 
 var postOne = new Post({
+  id: 1,
   title: "First post",
   content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem consequuntur porro id magni quos eaque rerum blanditiis, placeat praesentium, error aut sequi quidem. Dolorem laudantium asperiores, aspernatur non sunt corrupti."
 });
 
 var postTwo = new Post({
-  title: "Startups post"
+  id: 2,
+  title: "Startups post",
   content: "Something about startups"
 });
 
-var blog = new Posts( postOne );
+var blog = new Posts( [postOne, postTwo] );
 
 blog.add({
+  id: 3,
   author: "Zero",
   title: "Game theory",
   content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis repellat, voluptatem, maxime perferendis voluptas laudantium excepturi tenetur debitis deleniti dolorem sapiente cumque, impedit molestias suscipit, eaque maiores odit nesciunt eos."
 });
+
+var AppView = Backbone.View.extend({
+  el: '#main',
+  render: function () {
+    var appViewHTML = $('#appView').html();
+    this.$el.html( appViewHTML );
+
+    this.collection.each(function (post) {
+      var postListView = new PostListView({model: post});
+      postListView.render();
+    });
+  }
+});
+
+var PostListView = Backbone.View.extend({
+  tagName: 'li',
+  events: {
+    'click': 'showPost'
+  },
+  showPost: function () {
+    router.navigate('posts/' + this.model.get('id'), true);
+  },
+  render: function () {
+    this.$el.text( this.model.get('title') );
+    this.$el.appendTo('#posts');
+  }
+});
+
+var PostView = Backbone.View.extend({
+  el: '#main',
+  render: function () {
+    var postViewTemplater = _.template( $('#postView').html() );
+    this.$el.html( postViewTemplater( this.model.toJSON() ) );
+  }
+});
+
+var AppRouter = Backbone.Router.extend({
+  routes: {
+    '': 'index',
+    'posts/:id': 'viewPost'
+  },
+
+  index: function () {
+    var appView = new AppView({collection: blog});
+    appView.render();
+  },
+
+  viewPost: function (id) {
+    var post = blog.get(id);
+    var postView = new PostView({model: post});
+    postView.render();
+  }
+});
+
+var router = new AppRouter();
+$(document).ready(function () {
+  Backbone.history.start();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
