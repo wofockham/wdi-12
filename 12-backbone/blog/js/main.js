f8f1eafa8bd51a5afc99141df0ec013f20873a6f
Cold Head <certainthings@gmail.com>
2016-01-08 15:24:30 +1100

    more seed data

--- 12-backbone/blog/js/main.js
+++ 12-backbone/blog/js/main.js
@@ -1,6 +1,7 @@
 var Post = Backbone.Model.extend({
   defaults: {
-    author: "Terry"
+    author: "Terry",
+    title: "Untitled"
   },
 
   initialize: function () {
@@ -10,10 +11,6 @@ var Post = Backbone.Model.extend({
       console.log( "A posts author was changed" );
     });
   }
-});
-
-var postOne = new Post({
-  content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem consequuntur porro id magni quos eaque rerum blanditiis, placeat praesentium, error aut sequi quidem. Dolorem laudantium asperiores, aspernatur non sunt corrupti."
 });
 
 var Posts = Backbone.Collection.extend({
@@ -27,9 +24,22 @@ var Posts = Backbone.Collection.extend({
   }
 });
 
+var postOne = new Post({
+  title: "First post",
+  content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem consequuntur porro id magni quos eaque rerum blanditiis, placeat praesentium, error aut sequi quidem. Dolorem laudantium asperiores, aspernatur non sunt corrupti."
+});
+
+var postTwo = new Post({
+  title: "Startups post"
+  content: "Something about startups"
+});
+
 var blog = new Posts( postOne );
 
 blog.add({
   author: "Zero",
+  title: "Game theory",
   content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis repellat, voluptatem, maxime perferendis voluptas laudantium excepturi tenetur debitis deleniti dolorem sapiente cumque, impedit molestias suscipit, eaque maiores odit nesciunt eos."
 });
+
+
