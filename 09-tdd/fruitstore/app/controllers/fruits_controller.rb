cb4d9f570d76a44cd096a11c64c769ca33f2a5b6
Cold Head <certainthings@gmail.com>
2016-01-28 14:19:17 +1100

    tests for fruits#create

--- 09-tdd/fruitstore/app/controllers/fruits_controller.rb
+++ 09-tdd/fruitstore/app/controllers/fruits_controller.rb
@@ -1,5 +1,10 @@
 class FruitsController < ApplicationController
   def index
     @fruits = Fruit.all.reverse
+
+    respond_to do |format|
+      format.html # Fallback to the default action of rendering the index.html.erb
+      format.json { render :json => @fruits }
+    end
   end
 end
